f4095d10b8ece44e2bece8a64c911acf65913cf3
Todd Oakley <oakley@lifesci.ucsb.edu>
2017-11-19 15:05:50 -0800

    Added additional data columns for points

--- chart_contours.py
+++ chart_contours.py
@@ -6,6 +6,8 @@ import os
 import time
 import numpy as np
 
+minArea = 0
+
 def adjust_clip(image, black=0, white=255):
 	# build a lookup table mapping the pixel values [0, 255] to
 	# set values below black to 0 and above white to 255
@@ -29,9 +31,12 @@ def adjust_gamma(image, gamma=1.0):
 
 # Construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-ext", "--extension", required=False, default='png', help="extension name. default is 'png'.")
-ap.add_argument("-p", "--path", required=False, default='.', help="path to video frame files default is ./")
-ap.add_argument("-t", "--time", required=False, type = float, default=0.033333333, help="Time between frames. Default is 1/30 s")
+ap.add_argument("-ext", "--extension", required=False, default='png', 
+        help="extension name. default is 'png'.")
+ap.add_argument("-p", "--path", required=False, default='.', 
+        help="path to video frame files default is ./")
+ap.add_argument("-t", "--time", required=False, type = float, default=0.033333333, 
+        help="Time between frames. Default is 1/30 s")
 ap.add_argument("-g", "--gamma", required=True, type=float,
 	help="value of gamma")
 ap.add_argument("-b", "--black", required=True, type=int,
@@ -59,7 +64,7 @@ images.sort()
 
 
 #Print Header
-print("Frame"+","+"X"+","+"Y")
+print("frame,x,y,area,minI,maxI,meanI" )
 
 for image in images:
     image_path = os.path.join(dir_path, image)
@@ -74,15 +79,12 @@ for image in images:
     imgray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     ret,thresh = cv2.threshold(imgray,black,white,0)
     _, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
     contourcount = 0
-
-
-    #Now count the contours
-    thecX = 0
-    thecY = 0
-    themeanval = 0
+    cX = 0
+    cY = 0
     for c in contours:
-	if cv2.contourArea(c) > 0:
+	if cv2.contourArea(c) > minArea:
              contourcount = contourcount + 1
              M = cv2.moments(c)
              cX = int(M["m10"] / M["m00"])
@@ -92,42 +94,20 @@ for image in images:
              cv2.drawContours(mask,[c],0,255,-1)
              mean_val = cv2.mean(frame,mask = mask)
 	     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(imgray,mask = mask)
-
-             #This is adding each centroid to a list
-             cv2.rectangle(adjusted, (cX-10,cY-10), (cX+10,cY+10), (155, 0, 0), 2)
-
-	     #strip the suffix off the file name to leave just frame number
              image = image.strip('.png')
+             print(image + "," + str(cX) + "," + str(400-cY) + "," + str(cv2.contourArea(c) ) + "," + 
+	         str(min_val) + "," + str(max_val) + "," + str(mean_val[0]) )
 
-	     print(image+","+str(cX)+","+str(400-cY))
-
-#	     #Keep just the pulse around 600 for now
-#             if cX > 40 and cX < 60:
-#                  thecX = cX
-#                  thecY = cY
-#		  themeanval = mean_val[0]
-#                  cv2.rectangle(adjusted, (45,265), (65,285), (155, 0, 0), 2)
-#                  print("x50"+ "\t" + str(image) + "\t" + str(themeanval))
-#
-#             if cX > 80 and cX < 100:
-#                  thecX = cX
-#                  thecY = cY
-#		  themeanval = mean_val[0]
-#		  cv2.rectangle(adjusted, (90,240), (110,260), (155, 0, 0), 2)
-#                  print("x80"+ "\t" + str(image) + "\t" + str(themeanval))
-#
-
+#             cv2.drawContours(adjusted,contours,-1,(255,180,10),-1)
+#        else:
+#             cv2.drawContours(adjusted,contours,-1,(0,0,0),-1)
+             
+    #strip the suffix off the file name to leave just frame number
 
     #write the frame number for viewing
+    image = image.strip('.png')
     frametext = image
-    cv2.putText(adjusted,frametext, (30,450), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
-
-    frametext3 = str(contourcount)+" x="+ str(thecX) + " y=" + str(thecY) + " mI=" + str(themeanval)
-    cv2.putText(adjusted,frametext3, (30,400), cv2.FONT_HERSHEY_SIMPLEX, .5, (255,255,255))
-#    cv2.putText(adjusted,'0', (50,278), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
-#    cv2.putText(adjusted,'x', (90,250), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
-
-
+    cv2.putText(adjusted,frametext, (30,450), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,180,10))
     cv2.imshow('video',adjusted)
     if (cv2.waitKey(1) & 0xFF) == ord('q'): # Hit `q` to exit
         print(image)
--- classify.pl
+++ classify.pl
@@ -6,6 +6,7 @@ my $tmax = 3;	#max number of frames before considering a new pulse
 my $dmax = 5;	#max distance in pixels before considering a new pulse
 
 my $maxc = 500; #max contours/frame to cycle back through. Usually not used, but could be an issue when many pulses are on a frame at once.
+my $cols = 7; #Number of data columns in input file
 
 
 #*********** subroutine to calculate Euclidean distance between pulses
@@ -27,8 +28,7 @@ my $pulseno = 1;
 my $pulseofn = 0;
 my $closestcon = 10000; #Starting with absurdly high value so first d is always lower
 
-
-print "frame,x,y,pulse\n";
+print "frame,x,y,area,minI,maxI,meanI,pulse\n";
 #Full data matrix into 2d array
 while (my $row = <$fh>) {
   chomp $row;
@@ -49,51 +49,51 @@ for(my $i=0; $i < $counter-1 ; $i++) {
 		#Check if reached all the way back to Contour 0
 		if ($tp >= $i){
 			if($closestcon > 999){	#First contour which gets placed into a pulse 0 automatically
-				$contour[$i][3] = $pulseno;
-				for(my $d=0; $d < 3; $d++){
+				$contour[$i][$cols] = $pulseno;
+				for(my $d=0; $d < $cols; $d++){
 					print "$contour[$i][$d],";
 				}
 			}else{
 				#hit 0 but not first time through
 				$cdist = distance($contour[$i][1],$contour[$i][2], $contour[$i-$tp][1],$contour[$i-$tp][2]);
 				if ($cdist < $dmax){	#its the same pulse
-					$contour[$i][3] = $contour[$i-$tp][3];
-					for(my $d=0; $d < 3; $d++){
+					$contour[$i][$cols] = $contour[$i-$tp][$cols];
+					for(my $d=0; $d < $cols; $d++){
 						print "$contour[$i][$d],";
 					}
 					$closestcon = 600;
-					print "$contour[$i][3]\n";
+					print "$contour[$i][$cols]\n";
 					last;
 				}else{	#it's a new pulse
 					$pulseno++;
-					$contour[$i][3] = $pulseno;
-					for(my $d=0; $d < 3; $d++){
+					$contour[$i][$cols] = $pulseno;
+					for(my $d=0; $d < $cols; $d++){
 						print "$contour[$i][$d],";
 					}
 				$closestcon = 600;
-				print "$contour[$i][3]\n";
+				print "$contour[$i][$cols]\n";
 				last;
 				}
 			}
-			print "$contour[$i][3]\n";
+			print "$contour[$i][$cols]\n";
 			$closestcon = $dmax;
 			last;
 		}
 		#Now check distance of current contour to $tp previous contour 
 		$cdist = distance($contour[$i][1],$contour[$i][2], $contour[$i-$tp][1],$contour[$i-$tp][2]);
 		if ($cdist < $dmax){ #it's within the distance to be the same pulse
-			$pulseofn = $contour[$i-$tp][3];
-			$contour[$i][3] = $pulseofn;
-			for(my $d=0; $d < 3; $d++){
+			$pulseofn = $contour[$i-$tp][$cols];
+			$contour[$i][$cols] = $pulseofn;
+			for(my $d=0; $d < $cols; $d++){
 				print "$contour[$i][$d],";
 			}
-			print "$contour[$i][3]\n";
+			print "$contour[$i][$cols]\n";
 			$closestcon = $dmax;
 			last;
 		}
 		if ($cdist < $closestcon){
 			$closestcon = $cdist;
-			$pulseofn = $contour[$i-$tp][3];
+			$pulseofn = $contour[$i-$tp][$cols];
 		}
 #Uncomment to Debug
 #		print "i".$i.")  $tp Contours ago: ".$contour[$i-$tp][0].",".$contour[$i-$tp][1]." Current frame: $contour[$i][0],$contour[$i][1]\t";
@@ -101,21 +101,21 @@ for(my $i=0; $i < $counter-1 ; $i++) {
 		if ($contour[$i][0] - $contour[$i-$tp][0] > $tmax){
 #			print "More than $tmax frames ago. Closest dist is $closestcon\n\n";
 			if ($cdist < $dmax){	#its the same pulse
-				$contour[$i][3] = $pulseofn;
-				for(my $d=0; $d < 3; $d++){
+				$contour[$i][$cols] = $pulseofn;
+				for(my $d=0; $d < $cols; $d++){
 					print "$contour[$i][$d],";
 				}
 				$closestcon = 600;
-				print "$contour[$i][3]\n";
+				print "$contour[$i][$cols]\n";
 				last;
 			}else{	#it's a new pulse
 				$pulseno++;
-				$contour[$i][3] = $pulseno;
-				for(my $d=0; $d < 3; $d++){
+				$contour[$i][$cols] = $pulseno;
+				for(my $d=0; $d < $cols; $d++){
 					print "$contour[$i][$d],";
 				}
 				$closestcon = 600;
-				print "$contour[$i][3]\n";
+				print "$contour[$i][$cols]\n";
 				last;
 			}
 		}
