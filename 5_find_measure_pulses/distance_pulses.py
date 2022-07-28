c2af352c128d95b1f64d10d292d24b1f0db7de5c
Todd Oakley <oakley@lifesci.ucsb.edu>
2022-07-27 18:56:26 -0700

    progress on distance pulse  segmentation script

--- 5_find_measure_pulses/distance_pulses.py
+++ 5_find_measure_pulses/distance_pulses.py
@@ -15,11 +15,16 @@ ap.add_argument("-f", "--file", required=True, type=str,
 args = vars(ap.parse_args())
 file = args["file"]
 
-XMAX = 15	#max distance in X to be still considered the same pulse
-YMAX = 15
+
+#Parameters for segmenting into pulses
+XMAX = 5	#max distance in X to be still considered the same pulse
+YMAX = 5
 
 SRMAX = 5	#max difference for correspondence of Y axis, assuming stereo rectification aligns Y
-HPP = 0.25 	#hot pixel proportion = percentage of frames that contain a contour at the same X-axis value to be considered noise
+HPP = 0.15 	#hot pixel proportion = percentage of frames that contain a contour at the same X-axis value to be considered noise
+HPD = 2		#hot pixel distance (number of pixels +/- ) from defined hot pixels
+
+PDMIN = 3 	#minimum number of frames for pulse duration
 
 
 table = pd.read_csv(file, delimiter = '\t')
@@ -101,34 +106,89 @@ for index, row in pulses.iterrows():
 finalpulses = finalpulses.sort_values(['pulse', 'frame', 'camera', 'cX', 'cY'])
 #finalpulses = finalpulses.reset_index(drop=True)	#need to re-index df to use index later in order of sorted rows
 
-xmode = finalpulses.mode()['cX'][0]			#find most common X-axis value
+xmode = finalpulses.mode()['cX'][0]					#find most common X-axis value
 uframes = len(pd.unique(finalpulses['frame']))		#count total number of unique frames
+noisepulses = 0
 
-Nxmode = finalpulses['cX'].value_counts()[xmode]	
-Nxmode1 = finalpulses['cX'].value_counts()[xmode+1]	#Find number of frames within +/- 1 of most common value
-Nxmodem1 = finalpulses['cX'].value_counts()[xmode-1]
+#Should probably separate out L and R cameras and do them separately
+noisepulses = noisepulses + finalpulses['cX'].value_counts()[xmode]	#count total number of times most common X value is found
+
+#I think there is a mistake here because it will count x-1 twice
+for i in range(1, HPD + 1):
+    if xmode + i in finalpulses['cX']:
+        noisepulses = noisepulses + finalpulses['cX'].value_counts()[xmode + i] 	#Find number of frames within +/- HPD of most common value
+    if xmode - i in finalpulses['cX']:
+        noisepulses = noisepulses + finalpulses['cX'].value_counts()[xmode - i]
+
 
 #Replace pulse name with 'noise' for most common x-axis value, if it is above the HPP threshold defining it as Hot Pixel noise
-if(Nxmode + Nxmode1 + Nxmodem1 > HPP) :
+if(noisepulses/uframes > HPP) :
     finalpulses.loc[finalpulses.cX == xmode, 'pulse'] = "noise"
-    finalpulses.loc[finalpulses.cX == xmode + 1, 'pulse'] = "noise"
-    finalpulses.loc[finalpulses.cX == xmode - 1, 'pulse'] = "noise"
+    for i in range(1, HPD + 1) :
+        finalpulses.loc[finalpulses.cX == xmode + i, 'pulse'] = "noise"
+        finalpulses.loc[finalpulses.cX == xmode - i, 'pulse'] = "noise"
+        finalpulses.loc[finalpulses.cX == xmode, 'pulse'] = "noise"
+        finalpulses.loc[finalpulses.cX == xmode + 1, 'pulse'] = "noise"
+        finalpulses.loc[finalpulses.cX == xmode - 1, 'pulse'] = "noise"
+
+#Visualize before noise
 
 
 
-#Visualize
-#print(finalpulses.to_string())
+#Next define as noise pulses that are too short to be a pulse
+unique_pulses = finalpulses['pulse'].unique()
+for rezy in unique_pulses:
+    df_single_pulse = finalpulses[finalpulses['pulse']==rezy]
+    if df_single_pulse['cX'].count() < PDMIN :
+        print(rezy + " is too short to be a pulse based on PDMIN = " + str(PDMIN))
+        finalpulses.loc[finalpulses.pulse == rezy, 'pulse'] = "too_short"
+
 
 noise = finalpulses.loc[finalpulses['pulse'] == 'noise']
-finalpulses = finalpulses.loc[finalpulses['pulse'] != 'noise']
+too_short = finalpulses.loc[finalpulses['pulse'] == 'too_short']
+denoised = finalpulses.loc[finalpulses['pulse'] != 'noise']
+denoised = denoised.loc[finalpulses['pulse'] != 'too_short']
 
 
-color_dict = dict({'noise':'gray'})
+#Finally, find most probable stereo pairs based on correspondence of frame start/stop, correspondence of Y-axis point, and possibly X-axis disparity
+#Working with only denoised data at this point
+#create a new df with information about entire pulses instead of frame-by frame
+#df_pulse_sums = pd.DataFrame(columns=['camera','pulse','start', 'finish', 'minx', 'maxx', 'miny', 'maxy'])
+df_pulse_sums = pd.DataFrame(columns=['camera','pulse'], index = ['0', '1'])
+pulse_frames = denoised['pulse'].unique()
+dfrows = 0
+for rezy in pulse_frames:
+    df_frames = denoised[denoised['pulse']==rezy]
+    print("rezy: " + rezy)
+    #df_pulse_sums[i] = [df_frames['camera'].mode(), rezy,  df_frames['frame'].min(), df_frames['frame'].max(), df_frames['cX'].min(), df_frames['cX'].max(), df_frames['cY'].min(), df_frames['cY'].max() ]
+    df_pulse_sums[dfrows] = ['left', 'p8']
+    dfrows = dfrows + 1
+print(df_pulse_sums)
+        #print(rezy + " is too short to be a pulse based on PDMIN = " + str(PDMIN))
+        #finalpulses.loc[finalpulses.pulse == rezy, 'pulse'] = "too_short"
+
+#Divide into L and R camera data frames
+df_left = denoised[denoised['camera']=='left']
+df_right = denoised[denoised['camera']=='right']
+df_left = pulses.reset_index(drop=True)	
+df_right = pulses.reset_index(drop=True)	
+
+#print(df_left.to_string())
+#print(df_right.to_string())
+
+
+
+
+#Visualize after removing noise
+#print(finalpulses.to_string())
 
 fig, axs = plt.subplots(ncols=2)
-#sns.scatterplot(x='frame', y='cX', data=finalpulses, hue='pulse', edgecolor = 'none', ax=axs[0], legend = False)
-sns.scatterplot(x='frame', y='cY', data=finalpulses, hue='pulse', edgecolor = 'none', ax=axs[1], legend = False)
-sns.scatterplot(x='frame', y='cX', data=noise, edgecolor = 'black', ax=axs[0], legend = False, palette=color_dict)
+sns.scatterplot(x='frame', y='cX', data=denoised, hue='pulse', edgecolor = 'none', ax=axs[0], legend = False)
+axs[0].set_ylim(1, 640)
+sns.scatterplot(x='frame', y='cY', data=denoised, hue='pulse', edgecolor = 'none', ax=axs[1], legend = False)
+axs[1].set_ylim(1, 480)
+
+##sns.scatterplot(x='frame', y='cX', data=noise, edgecolor = 'black', ax=axs[0], legend = False)
 
 plt.show()
 
--- 5_find_measure_pulses/visualize_contours.py
+++ 5_find_measure_pulses/visualize_contours.py
@@ -15,9 +15,21 @@ file = args["file"]
 
 table = pd.read_csv(file, delimiter = '\t')
 
+##2d
 fig, axs = plt.subplots(ncols=2)
 sns.scatterplot(x='frame', y='cX', data=table, hue='camera', edgecolor = 'none', ax=axs[0])
-sns.scatterplot(x='frame', y='cY', data=table, hue='camera', edgecolor = 'none', ax=axs[1])
+sns.scatterplot(x='frame', y='cY', data=table, hue='camera', edgecolor = 'none', ax=axs[1], legend=False)
+
+##3d
+#plt.figure(figsize=(6,5))
+#axes = plt.axes(projection='3d')
+#print(type(axes))
+#axes.scatter3D(table['frame'], table['cX'], table['cY'], s=10)
+
+#axes.set_xlabel('frame')
+#axes.set_ylabel('x position')
+#axes.set_zlabel('y position')
+
 
 plt.show()
 
